f47b07a8eed249c7f7d9d85ffadd02d8a31ab549
ben aicha iheb <benaichaihebb9@gmail.com>
2022-08-24 23:18:20 +0100

    first commit

---
+++ client/src/components/featured/Featured.jsx
@@ -0,0 +1,55 @@
+import useFetch from "../../hooks/useFetch";
+import "./featured.css";
+
+const Featured = () => {
+  const { data, loading, error } = useFetch(
+    "/hotels/countByCity?cities=berlin,madrid,london"
+  );
+
+  return (
+    <div className="featured">
+      {loading ? (
+        "Loading please wait"
+      ) : (
+        <>
+          <div className="featuredItem">
+            <img
+              src="https://mediap.flypgs.com/awh/578/294//files/ucak-biletleri/sehirler/berlin/desktop.jpg"
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>Berlin</h1>
+              <h2>{data[0]} properties</h2>
+            </div>
+          </div>
+
+          <div className="featuredItem">
+            <img
+              src="https://media.architecturaldigest.com/photos/57ad893acfc37bc171ad8082/2:1/w_5122,h_2561,c_limit/madrid-travel-guide.jpg"
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>Madrid</h1>
+              <h2>{data[1]} properties</h2>
+            </div>
+          </div>
+          <div className="featuredItem">
+            <img
+              src="https://www.london.ac.uk/sites/default/files/styles/max_1300x1300/public/2018-10/london-aerial-cityscape-river-thames_1.jpg?itok=6LenFxuz"
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>London</h1>
+              <h2>{data[2]} properties</h2>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Featured;
